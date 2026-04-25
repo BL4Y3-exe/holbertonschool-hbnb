0b78d8a41f1ab950b18e60ab1ec8f3e0d95e2962
Ilknur <ilknurkazimov308@gmail.com>
2026-04-25 19:46:01 +0400

    Add user endpoints

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 def create_app():
     app = Flask(__name__)
@@ -11,5 +12,7 @@ def create_app():
         description='HBnB Application API',
         doc='/api/v1/'
     )
+
+    api.add_namespace(users_ns, path='/api/v1/users')
 
     return app
