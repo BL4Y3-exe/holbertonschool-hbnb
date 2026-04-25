a3c23d862871c9c22c66783eb86059acf0274282
Ilknur <ilknurkazimov308@gmail.com>
2026-04-25 20:25:06 +0400

    Add place endpoints

---
+++ part2/hbnb/app/api/v1/places.py
@@ -0,0 +1,114 @@
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('places', description='Place operations')
+
+amenity_model = api.model('PlaceAmenity', {
+    'id': fields.String,
+    'name': fields.String
+})
+
+user_model = api.model('PlaceUser', {
+    'id': fields.String,
+    'first_name': fields.String,
+    'last_name': fields.String,
+    'email': fields.String
+})
+
+place_model = api.model('Place', {
+    'title': fields.String(required=True),
+    'description': fields.String,
+    'price': fields.Float(required=True),
+    'latitude': fields.Float(required=True),
+    'longitude': fields.Float(required=True),
+    'owner_id': fields.String(required=True),
+    'amenities': fields.List(fields.String)
+})
+
+@api.route('/')
+class PlaceList(Resource):
+
+    @api.expect(place_model, validate=True)
+    @api.response(201, 'Place created')
+    def post(self):
+        data = api.payload
+
+        try:
+            place = facade.create_place(data)
+
+            return {
+                "id": place.id,
+                "title": place.title,
+                "description": place.description,
+                "price": place.price,
+                "latitude": place.latitude,
+                "longitude": place.longitude,
+                "owner_id": place.owner.id
+            }, 201
+
+        except ValueError as e:
+            return {"error": str(e)}, 400
+
+
+    @api.response(200, 'List of places')
+    def get(self):
+        places = facade.get_all_places()
+
+        return [
+            {
+                "id": p.id,
+                "title": p.title,
+                "latitude": p.latitude,
+                "longitude": p.longitude
+            }
+            for p in places
+        ], 200
+    
+@api.route('/<place_id>')
+class PlaceResource(Resource):
+
+    @api.response(200, 'Place found')
+    @api.response(404, 'Not found')
+    def get(self, place_id):
+        place = facade.get_place(place_id)
+
+        if not place:
+            return {"error": "Place not found"}, 404
+
+        return {
+            "id": place.id,
+            "title": place.title,
+            "description": place.description,
+            "price": place.price,
+            "latitude": place.latitude,
+            "longitude": place.longitude,
+            "owner": {
+                "id": place.owner.id,
+                "first_name": place.owner.first_name,
+                "last_name": place.owner.last_name,
+                "email": place.owner.email
+            },
+            "amenities": [
+                {"id": a.id, "name": a.name}
+                for a in place.amenities
+            ]
+        }, 200
+
+
+    @api.expect(place_model, validate=True)
+    @api.response(200, 'Updated')
+    def put(self, place_id):
+        data = api.payload
+
+        try:
+            place = facade.update_place(place_id, data)
+
+            if not place:
+                return {"error": "Place not found"}, 404
+
+            return {
+                "message": "Place updated successfully"
+            }, 200
+
+        except ValueError as e:
+            return {"error": str(e)}, 400
--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,9 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.user import User
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -57,3 +60,52 @@ class HBnBFacade:
 
         self.amenity_repo.update(amenity_id, amenity_data)
         return amenity
+    
+    def create_place(self, place_data):
+        owner = self.user_repo.get(place_data["owner_id"])
+        if not owner:
+            raise ValueError("Owner not found")
+
+        amenities = []
+        for amenity_id in place_data.get("amenities", []):
+            amenity = self.amenity_repo.get(amenity_id)
+            if amenity:
+                amenities.append(amenity)
+
+        place_data["owner"] = owner
+        place_data["amenities"] = amenities
+
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+
+    def update_place(self, place_id, place_data):
+        place = self.get_place(place_id)
+        if not place:
+            return None
+
+        if "owner_id" in place_data:
+            owner = self.user_repo.get(place_data["owner_id"])
+            if not owner:
+                raise ValueError("Owner not found")
+            place_data["owner"] = owner
+
+        if "amenities" in place_data:
+            amenities = []
+            for a_id in place_data["amenities"]:
+                a = self.amenity_repo.get(a_id)
+                if a:
+                    amenities.append(a)
+            place_data["amenities"] = amenities
+
+        self.place_repo.update(place_id, place_data)
+        return place
